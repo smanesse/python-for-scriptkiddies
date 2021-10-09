d5796762926df66f75ec6aafb5f43b8b0dbb2e17
Seth Manesse <seth.manesse@contractpal.com>
2021-10-09 14:43:28 -0600

    fix naming (i promise I know how to use git....smh

--- http_req.py/headers.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://dummy.restapiexample.com/api/v1/create"
-my_data =   {"name":"test", "salary":"123", "age":"23"}
-my_cookies = {"PHPSESSID": "059BE6C6A783CD784"}
-my_headers = {"User-Agent":"python stuff"}
-response = requests.post(url, data=my_data, cookies=my_cookies, headers=my_headers)
-print(response.text)
